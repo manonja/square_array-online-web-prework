ed6a8fe9286144ba0b655ff45fc1b99fe192a9a7
manon jacquin <manon.jacquin@gmail.com>
2018-10-04 21:19:40 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each {|x| new_array.push(x ** 2)} 
+  new_array
   
-  new_array = array.each do {|x| x ** 2}
-  return new_array
-
     
-    # return array.collect {|x| x ** 2 }
+  # return array.collect {|x| x ** 2 }
 end
